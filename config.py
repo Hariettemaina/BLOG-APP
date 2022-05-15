92b812529f0de1a4d920a5e99721d5d0f78813b3
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-15 12:17:44 +0300

    create db,addusername and pssword

---
+++ config.py
@@ -0,0 +1,34 @@
+
+  
+import os 
+
+class Config:
+    SECRET_KEY = 'abcde'
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://mhariette@localhost/jblog"
+    UPLOADED_PHOTOS_DEST = "app/static/photos"
+
+    # email configurations
+    MAIL_SERVER = "smtp.gmail.com"
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
+    MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
+
+class ProdConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
+
+class TestConfig(Config):
+    
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://mhariette@localhost/jblog_test"
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = "postgresql+psycopg2://mhariette@localhost/jblog"
+    DEBUG = True
+
+
+config_options = {
+    "development": DevConfig,
+    "production": ProdConfig,
+    "test": TestConfig
+}
