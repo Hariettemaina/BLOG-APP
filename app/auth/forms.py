45a122dd171f0378bebb02872f10a11b239889f9
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-15 11:48:56 +0300

    validation of signup and login

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,7 +1,7 @@
 
   
 from flask_wtf import FlaskForm
-from wtforms import (StringField, PasswordField, SubmitField, ValidationError, BooleanField)
+from wtforms import StringField, PasswordField, SubmitField, ValidationError, BooleanField
 from wtforms.validators import InputRequired, Email, EqualTo
 from ..models import User
 
