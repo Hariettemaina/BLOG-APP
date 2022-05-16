21a7b55c021d7927e85df87196393f8e55fac88f
Hariettemaina <hariette.maina@student.moringaschool.com>
2022-05-16 08:31:53 +0300

    add migrations

---
+++ migrations/versions/8fce27986867_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8fce27986867
+Revises: 
+Create Date: 2022-05-16 08:29:24.013934
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8fce27986867'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
